4d61a55aecfc301149bcefbf6aa5cef8cb029ff5
marvinmarvin2089-source <marvinmarvin2089@gmail.com>
2026-02-04 20:30:47 -0300

    ajuste da imagem

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ const fromValue = document.querySelector('#from-value');
 const toValue = document.querySelector('#to-value');
 const toCurrency = document.querySelector('#to-currency');
 const toName = document.querySelector('#to-name');
+const toImage = document.querySelector('.moeda-dolares');
 
 // taxas fixas (exemplo)
 const dolar = 5.0;
@@ -18,23 +19,30 @@ function convertValues() {
         return;
     }
 
+    // valor em reais
     fromValue.innerHTML = value.toLocaleString('pt-BR', {
         style: 'currency',
         currency: 'BRL'
     });
 
+    // dólar
     if (toCurrency.value === 'USD') {
         const converted = value / dolar;
         toName.innerHTML = 'Dólar';
+        toImage.src = './assets/dola.png';
+        toImage.alt = 'Bandeira dos Estados Unidos';
         toValue.innerHTML = converted.toLocaleString('pt-BR', {
             style: 'currency',
             currency: 'USD'
         });
     }
 
+    // euro
     if (toCurrency.value === 'EUR') {
         const converted = value / euro;
         toName.innerHTML = 'Euro';
+        toImage.src = './assets/euro.jpg';
+        toImage.alt = 'Bandeira da União Europeia';
         toValue.innerHTML = converted.toLocaleString('pt-BR', {
             style: 'currency',
             currency: 'EUR'
